6f7367a2f62371a65a7a9bdd7bbf3694edf9f60f
Artur Podsiadły <artpods56@gmail.com>
2025-09-07 12:37:37 +0000

    feat: add toggleterm and neogit plugins

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,3 +4,8 @@ local map = vim.keymap.set
 
 map("n", "<leader>qq", ":qa<cr>", { desc = "Quit all" })
 
+--- Neogit --- /plugins/essentials.lua
+
+map("n", "<leader>gg", function()
+	require("neogit").open({ cwd = LazyVim.root.git() })
+end, { desc = "Neogit (Root Dir)" })
--- lua/plugins/essentials.lua
+++ lua/plugins/essentials.lua
@@ -1,25 +1,56 @@
 -- Essentials: treesitter, LSP base, formatting, finder
 
 return {
-  -- Treesitter
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    opts = {
-      highlight = { enable = true },
-      indent = { enable = true },
-      ensure_installed = { "lua", "vim", "vimdoc", "bash", "markdown", "markdown_inline", "json", "yaml", "toml" },
-    },
-  },
+	-- Treesitter
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		opts = {
+			highlight = { enable = true },
+			indent = { enable = true },
+			ensure_installed = { "lua", "vim", "vimdoc", "bash", "markdown", "markdown_inline", "json", "yaml", "toml" },
+		},
+	},
 
-  -- LSP config (LazyVim will install mason & base servers)
-  { "neovim/nvim-lspconfig" },
+	-- LSP config (LazyVim will install mason & base servers)
+	{ "neovim/nvim-lspconfig" },
 
-  -- Formatting & linting via conform and nvim-lint (already in LazyVim but ok to keep)
-  { "stevearc/conform.nvim" },
-  { "mfussenegger/nvim-lint" },
+	-- Formatting & linting via conform and nvim-lint (already in LazyVim but ok to keep)
+	{ "stevearc/conform.nvim" },
+	{ "mfussenegger/nvim-lint" },
 
-  -- Finder (fzf-lua is lightweight and fast)
-  { "ibhagwan/fzf-lua" },
+	-- Finder (fzf-lua is lightweight and fast)
+	{ "ibhagwan/fzf-lua" },
+
+	--- ToggleTerm
+	{
+		"akinsho/toggleterm.nvim",
+		config = true,
+		cmd = "ToggleTerm",
+		keys = {
+			{ "§", "<cmd>ToggleTerm<cr>", desc = "Toggle floating terminal on MacOS" },
+			{ "<F2>", "<cmd>ToggleTerm<cr>", desc = "Toggle floating terminal on Windows" },
+		},
+		opts = {
+			open_mapping = { [[§]], [[<F2>]] },
+			direction = "float",
+			shade_filetypes = {},
+			hide_numbers = true,
+			insert_mappings = true,
+			terminal_mappings = true,
+			start_in_insert = true,
+			close_on_exit = true,
+		},
+	},
+
+	--- NeoGit
+	{
+		"NeogitOrg/neogit",
+		dependencies = {
+			"nvim-lua/plenary.nvim", -- required
+			"sindrets/diffview.nvim", -- optional - Diff integration
+			"ibhagwan/fzf-lua", -- optional
+		},
+		config = true,
+	},
 }
-
